24ce50c8327866a1555ab6cd791f282c628761fd
NightFurySL2001 <33471049+NightFurySL2001@users.noreply.github.com>
2025-10-18 23:37:00 +0800

    fix docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,17 +1,21 @@
 # 更新日志 Changelog
 
-
 ## 械黑 GB Frex Sans GB v1.100 （2025/10/10）
 
-* 应上流厂商威胁，本计划西文名称由 "Flex Sans" 更改为 "Frex Sans"。更新字体前建议删除旧字体文件以确认使用最新版本。  
-  Due to the threat from upstream font foundry, this project has changed name from "Flex Sans" to "Frex Sans". Please delete the old version before updating to ensure using the latest version.
-* 将 OpenType `ss02` 特性映射至单层拉丁小写 g。  
-  Map Opentype `ss02` feature to single-storey lowercase Latin g.
-* 修改部分插值错误汉字。  
-  Fix some interpolation bugs.
-* 新增部分汉字，未根据任何字表添加。  
-  New ideographs (not following any existing character tables).
+-   应上流厂商威胁，本计划西文名称由 "Flex Sans" 更改为 "Frex Sans"。更新字体前建议删除旧字体文件以确认使用最新版本。  
+    Due to the threat from upstream font foundry, this project has changed name from "Flex Sans" to "Frex Sans". Please delete the old version before updating to ensure using the latest version.
+-   将 OpenType `ss02` 特性映射至单层拉丁小写 g。  
+    Map Opentype `ss02` feature to single-storey lowercase Latin g.
+-   修改部分插值错误汉字。  
+    Fix some interpolation bugs.
+-   新增部分汉字，未根据任何字表添加。  
+    New ideographs (not following any existing character tables).
+-   VF 版：修改未支援可变字体环境默认使用 Regular 字重。  
+    VF version: Change the default weight to Regular in unsupported environment.
 
+> [!NOTE]  
+> Windows 预览有反馈出现汉字宽度异常，是 Windows 的问题而非本字体问题。日常使用不会受影响。  
+> Windows font viewer may display incorrect spacing for hanzi, this is a problem with Windows and not the font. This will not affect daily usage.
 
 ## 械黑 GB Flex Sans GB v1.000 （2025/07/01）
 
--- sources/build.ps1
+++ sources/build.ps1
@@ -1,36 +1,36 @@
-# cd frex-sc-src
-# git pull
-# cd ..
+cd frex-sc-src
+git pull
+cd ..
 
-# echo "Cleaning up old UFOs..."
-# Remove-Item -Path frex-sc-han/han_Bold.ufo -Recurse -Force -ErrorAction SilentlyContinue
-# Remove-Item -Path frex-sc-han/han_Regular.ufo -Recurse -Force -ErrorAction SilentlyContinue
-# Remove-Item -Path frex-sc-han/han_Thin.ufo -Recurse -Force -ErrorAction SilentlyContinue
-# Remove-Item -Path FrexSansGBVF/masters/han_Bold.ufo -Recurse -Force -ErrorAction SilentlyContinue
-# Remove-Item -Path FrexSansGBVF/masters/han_Regular.ufo -Recurse -Force -ErrorAction SilentlyContinue
-# Remove-Item -Path FrexSansGBVF/masters/han_Thin.ufo -Recurse -Force -ErrorAction SilentlyContinue
-# Remove-Item -Path FrexSansGBVF/masters/Bold.ufo -Recurse -Force -ErrorAction SilentlyContinue
-# Remove-Item -Path FrexSansGBVF/masters/Regular.ufo -Recurse -Force -ErrorAction SilentlyContinue
-# Remove-Item -Path FrexSansGBVF/masters/Thin.ufo -Recurse -Force -ErrorAction SilentlyContinue
+echo "Cleaning up old UFOs..."
+Remove-Item -Path frex-sc-han/han_Bold.ufo -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path frex-sc-han/han_Regular.ufo -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path frex-sc-han/han_Thin.ufo -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path FrexSansGBVF/masters/han_Bold.ufo -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path FrexSansGBVF/masters/han_Regular.ufo -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path FrexSansGBVF/masters/han_Thin.ufo -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path FrexSansGBVF/masters/Bold.ufo -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path FrexSansGBVF/masters/Regular.ufo -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path FrexSansGBVF/masters/Thin.ufo -Recurse -Force -ErrorAction SilentlyContinue
 
-# # require installing fontra and fontra-rcjk in venv
-# echo "Converting RCJK to designspace..."
-# mkdir -Force frex-sc-han
-# fontra-copy export-rcjk.yaml frex-sc-han\han.designspace
+# require installing fontra and fontra-rcjk in venv
+echo "Converting RCJK to designspace..."
+mkdir -Force frex-sc-han
+fontra-copy export-rcjk.yaml frex-sc-han\han.designspace
 
-# echo "Copying RCJK UFOs to FrexSansGB masters..."
-# Copy-Item "frex-sc-han\han_Bold.ufo" "FrexSansGBVF\masters\han_Bold.ufo" -Recurse -Force
-# Copy-Item "frex-sc-han\han_Regular.ufo" "FrexSansGBVF\masters\han_Regular.ufo" -Recurse -Force
-# Copy-Item "frex-sc-han\han_Thin.ufo" "FrexSansGBVF\masters\han_Thin.ufo" -Recurse -Force
+echo "Copying RCJK UFOs to FrexSansGB masters..."
+Copy-Item "frex-sc-han\han_Bold.ufo" "FrexSansGBVF\masters\han_Bold.ufo" -Recurse -Force
+Copy-Item "frex-sc-han\han_Regular.ufo" "FrexSansGBVF\masters\han_Regular.ufo" -Recurse -Force
+Copy-Item "frex-sc-han\han_Thin.ufo" "FrexSansGBVF\masters\han_Thin.ufo" -Recurse -Force
 
-# cd "FrexSansGBVF\masters"
+cd "FrexSansGBVF\masters"
 
-# echo "Merging FrexSansGB Latin and CJK UFOs..."
-# ufomerge --skip-existing --layout-closure --output Bold.ufo "FrexSansGBVF-Bold.ufo\" "han_Bold.ufo"
-# ufomerge --skip-existing --layout-closure --output Regular.ufo "FrexSansGBVF-Regular.ufo\" "han_Regular.ufo"
-# ufomerge --skip-existing --layout-closure --output Thin.ufo "FrexSansGBVF-Thin.ufo\" "han_Thin.ufo"
+echo "Merging FrexSansGB Latin and CJK UFOs..."
+ufomerge --skip-existing --layout-closure --output Bold.ufo "FrexSansGBVF-Bold.ufo\" "han_Bold.ufo"
+ufomerge --skip-existing --layout-closure --output Regular.ufo "FrexSansGBVF-Regular.ufo\" "han_Regular.ufo"
+ufomerge --skip-existing --layout-closure --output Thin.ufo "FrexSansGBVF-Thin.ufo\" "han_Thin.ufo"
 
-# cd ..
+cd ..
 python add-ufo-vert-info.py
 python add-vs-selector.py
 
